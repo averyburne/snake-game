4c85753c28ce4b946b2cee2fa2603b586b414ed9
Avery Burne <burneav@gmail.com>
2021-07-08 18:55:18 -0400

    Fixed horizontal movement and game ending conditions
    Snake can now move up and down like it is supposed to (needed to add the dy value in the moveSnake function)
    Also game now ends when it hits the boundary condition (needed to add a check into main for each time the snake moves)

--- js/game-script.js
+++ js/game-script.js
@@ -1,13 +1,13 @@
 const snakeboard = document.getElementById("game-canvas")
 const snakeboardCtx = snakeboard.getContext('2d')
-let changingDirections = false
+// let changingDirections = false
 let dx = 10
 let dy = 0
 
-const board_border = 'black';
-const board_background = 'white';
-const snake_col = 'lightblue';
-const snake_border = 'darkblue';
+const boardBorder = 'black';
+const boardBackground = 'white';
+const snakeCol = 'lightblue';
+const snakeBorder = 'darkblue';
 
 let snake = [  {x: 200, y: 200},
   {x: 190, y: 200},
@@ -15,23 +15,25 @@ let snake = [  {x: 200, y: 200},
   {x: 170, y: 200},
   {x: 160, y: 200},];
 
-document.addEventListener("keydown", change_direction)
+document.addEventListener("keydown", changeDirection)
 
 
 function main() {
+  if (has_game_ended()) return;
   setTimeout(function onTick() {
     clearCanvas()
-    move_snake()
+    moveSnake()
     drawSnake()
+    console.log(snake[0].x)
     main()
    }, 100)
 }
 
 function clearCanvas() {
   //  Select the colour to fill the drawing
-  snakeboardCtx.fillStyle = board_background;
+  snakeboardCtx.fillStyle = boardBackground;
   //  Select the colour for the border of the canvas
-  snakeboardCtx.strokestyle = board_border;
+  snakeboardCtx.strokestyle = boardBorder;
   // Draw a "filled" rectangle to cover the entire canvas
   snakeboardCtx.fillRect(0, 0, snakeboard.width, snakeboard.height);
   // Draw a "border" around the entire canvas
@@ -46,20 +48,20 @@ function drawSnake()
 
 function drawSnakePart(snakePart) 
 {  
-  snakeboardCtx.fillStyle = 'lightblue';  
-  snakeboardCtx.strokestyle = 'darkblue';
+  snakeboardCtx.fillStyle = snakeCol;  
+  snakeboardCtx.strokestyle = snakeBorder;
   snakeboardCtx.fillRect(snakePart.x, snakePart.y, 10, 10);  
   snakeboardCtx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
  
-function move_snake() 
+function moveSnake() 
 {  
-  const head = {x: snake[0].x + dx, y: snake[0].y};
+  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
   snake.unshift(head);
   snake.pop();
 }
 
-function change_direction(event) 
+function changeDirection(event) 
 {  
    const LEFT_KEY = 37;
    const RIGHT_KEY = 39;
@@ -111,31 +113,31 @@ function has_game_ended()
   }
   const hitLeftWall = snake[0].x < 0;  
   const hitRightWall = snake[0].x > snakeboard.width - 10;
-  const hitToptWall = snake[0].y &lt; 0;
+  const hitToptWall = snake[0].y < 0;
   const hitBottomWall = snake[0].y > snakeboard.height - 10;
  
   return hitLeftWall ||  hitRightWall || hitToptWall || hitBottomWall
 }
 
-function random_food(min, max)
-{  
-   return Math.round((Math.random() * (max-min) + min) / 10) * 10;
-}
+// function random_food(min, max)
+// {  
+//    return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+// }
  
-function gen_food() 
-{  
-   food_x = random_food(0, snakeboard.width - 10);
-   food_y = random_food(0, snakeboard.height - 10);
-   snake.forEach(function has_snake_eaten_food(part) {
-        const has_eaten = part.x == food_x && part.y == food_y;
-        if (has_eaten) gen_food();
-      });
-}
+// function gen_food() 
+// {  
+//    food_x = random_food(0, snakeboard.width - 10);
+//    food_y = random_food(0, snakeboard.height - 10);
+//    snake.forEach(function has_snake_eaten_food(part) {
+//         const has_eaten = part.x == food_x && part.y == food_y;
+//         if (has_eaten) gen_food();
+//       });
+// }
 
-function drawFood()
-{
-      snakeboard_ctx.fillStyle = 'lightgreen;
-      snakeboard_ctx.strokestyle = 'darkgreen';
-      snakeboard_ctx.fillRect(food_x, food_y, 10, 10);
-      snakeboard_ctx.strokeRect(food_x, food_y, 10, 10);
-}
+// function drawFood()
+// {
+//       snakeboardCtx.fillStyle = 'lightgreen;
+//       snakeboardCtx.strokestyle = 'darkgreen';
+//       snakeboardCtx.fillRect(food_x, food_y, 10, 10);
+//       snakeboardCtx.strokeRect(food_x, food_y, 10, 10);
+// }
